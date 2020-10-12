85ff7eac409b7cc7518d4c215c68a61a022e5035
Mariano Griffa <marianogriffa@hotmail.com>
2020-10-12 12:12:17 -0300

    segundo commit del form

---
+++ js/form.js
@@ -0,0 +1,14 @@
+
+
+const expresiones = {
+
+  usuario: /^[a-zA-Z0-9\-\_]{4,16}$/ ,
+  nombre: /^[a-zA-ZÀ-ÿ\s]{2,40}$/,
+  contraseña: /^.{4, 30}$/,
+  correo:  /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  numero: /^\d{7,14}$/
+}
+ 
+
+console.log('funcionando!! soy la consola del navegador!!');
+
