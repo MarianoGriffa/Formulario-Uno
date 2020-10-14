0264ee69bb714efad443272e231506ba0af38811
Mariano Griffa <marianogriffa@hotmail.com>
2020-10-14 17:21:24 -0300

    Primer commit del js

--- js/form.js
+++ js/form.js
@@ -1,4 +1,7 @@
+console.log('funcionando!! soy la consola del navegador!!');
 
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
 
@@ -6,9 +9,27 @@ const expresiones = {
   nombre: /^[a-zA-ZÀ-ÿ\s]{2,40}$/,
   contraseña: /^.{4, 30}$/,
   correo:  /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-  numero: /^\d{7,14}$/
+  telefono: /^\d{7,14}$/
 }
+
+const campos  = {
+  usuario: false,
+  nombre: false,
+  password: false,
+  correo: false,
+  telefono: false
+};
+
+
+
  
+inputs.addEventListener((input) => {
+  input.addEventListener('keyup', validarFormulario);
+  input.addEventListener('blob', validarFormulario);
+})
 
-console.log('funcionando!! soy la consola del navegador!!');
 
+
+formulario.addEventListener('submit',  (ev) =>  {
+  ev.preventDefault()
+})
